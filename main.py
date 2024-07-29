8443582f927a7a563cb8dd83187c3fe61b4e2d01
SrivathsaAcharya7 <srivathsa.acharya@triconinfotech.com>
2024-07-29 23:29:41 +0530

    feat: add dockerfile and requirements file

--- main.py
+++
@@ -1,8 +0,0 @@
-# from fastapi import FastAPI
-# from pymongo import MongoClient
-#
-#
-# app = FastAPI()
-#
-# conn= MongoClient("mongodb+srv://srivathsaacharya167:fRaSCuR0HfyiGLBZ@cluster0.mkqthv0.mongodb.net/notes")
-#
