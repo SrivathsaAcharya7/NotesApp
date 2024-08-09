93047b9779fbbfe8fd18fadc990da87fe731c3c6
Srivathsa Acharya R <srivathsaacharya167@gmail.com>
2024-08-09 16:40:22 +0530

    Update index.py

--- index.py
+++ index.py
@@ -5,5 +5,5 @@ from fastapi.templating import Jinja2Templates
 app=FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates=Jinja2Templates(directory="templates")
-
+#indexfile
 app.include_router(note)
